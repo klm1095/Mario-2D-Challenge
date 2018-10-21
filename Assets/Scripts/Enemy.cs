9b6ef579127e2933f1b63a05b1fa2700dd909a5e
klm1095 <43013346+klm1095@users.noreply.github.com>
2018-10-21 13:15:47 -0400

    Mario 2D Assignment
    This is incomplete (Tier 1 and some of 2 completed)

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    private Rigidbody2D wallHitBox;
+    private float speed;
+   
+
+    //ground check
+    private bool wallHit;
+    public Transform groundcheck;
+    public float boxWidth;
+    public float boxHeight;
+    public LayerMask isGround;
+
+    // private float jumpTimeCounter;
+    //public float jumpTime;
+    //private bool isJumping;
+
+    //audio stuff
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        wallHitBox = GetComponent<Rigidbody2D>();
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        wallHit = Physics2D.OverlapBox(wallHitBox.position, new Vector2(boxWidth, boxHeight), 0, isGround);
+
+        if (wallHit == true)
+        {
+            speed = speed * -1;
+        }       
+    }
+
+   
+
+}
---
+++ Assets/Scripts/PlayerAnimate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimate : MonoBehaviour {
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("IsRun", true);
+        }
+
+        else
+        {
+            anim.SetBool("IsRun", false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("Jump");
+        }
+    }
+}
